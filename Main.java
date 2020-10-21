e2db0b7fb260865de7b8b309cbad71a330c8575a
schroeen <schroeen@mail.uc.edu>
2020-10-21 00:02:56 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+class Main {
+  public static void main(String[] args) {
+    Tree tree1 = new Tree();
+    Tree tree2 = new Tree("Maple","Broadleaf",true,"Red");
+
+    tree1.print();
+    tree2.print();
+  }
+}
